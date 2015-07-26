0b2d399af17e23b853492b16331b1118c626e5c8
David McVicar <dave.mcvicar@livingsocial.com>
2015-07-26 11:07:39 -0400

    fully functional game prompter.  pre-rename

--- prompt.rb
+++ prompt.rb
@@ -2,29 +2,53 @@ module Blackjack
   class Prompt
     attr_reader :legend
     attr_reader :valid_inputs
+    attr_reader :minimum_integer
+    attr_reader :maximum_integer
     attr_reader :input
     attr_reader :output
 
     def initialize(*valid_cmd_names)
       #
       # usage:
-      #   p = Prompt.new("Hit", "Stand", "sPlit", "Double")
+      #   p = Prompt.new("Bet Amount:int:1:10", "Hit", "Stand", "sPlit", "Double")
       #   p.prompt do |cmd|
       #     case cmd
-      #        when "Hit"
+      #        when "h"
       #           ....
-      #        when "sPlit"
+      #        when "p"
+      #        else
+      #          cmd.to_i
       #     end
       #   end
       #
       commands = []
-      @valid_inputs = []
+      @valid_inputs = ['q']
+      @minimum_integer = nil
+      @maximum_integer = nil
       valid_cmd_names.each do |cmd|
-        cap_let = cmd.match(/[A-Z]/)[0]
-        valid_inputs << cap_let.downcase
-        commands << cmd.gsub(/([A-Z])/, "[\\1]")
+        scmd = cmd.split(':')
+        nc = scmd.length
+        if nc > 1
+          case scmd[1]
+            when 'int'
+              @minimum_integer = scmd[2].to_i
+              if nc == 4
+                @maximum_integer = scmd[3].to_i
+                guide = " (#{minimum_integer} - #{maximum_integer})"
+              else
+                guide = " ( >= #{minimum_integer})"
+              end
+              commands << scmd[0] + guide 
+            else
+              raise "invalid command type"
+          end
+        else
+          cap_let = cmd.match(/[A-Z]/)[0]
+          valid_inputs << cap_let.downcase
+          commands << cmd.gsub(/([A-Z])/, "[\\1]")
+        end
       end
-      @legend = commands[0..-2].join(", ") + " or " + commands[-1]
+      @legend = commands.join(", ") + " or [Q]uit"
       @input = $stdin
       @output = $stdout
     end
@@ -36,10 +60,14 @@ module Blackjack
         println legend
         print "=> "
         cmd = getline
-        break if valid_inputs.include?(cmd.downcase)
+        next if cmd.empty?
+        dcmd = cmd.downcase
+        break if valid_cmd?(dcmd)
+        println "'#{cmd}' is invalid"
       end
-      yield cmd if block_given?
-      cmd
+      quit_check(dcmd)
+      yield dcmd if block_given?
+      dcmd
     end
 
     def getline
@@ -52,6 +80,23 @@ module Blackjack
 
     def println(str)
       output.puts str 
+    end
+
+    private
+
+    def quit_check(dcmd)
+      exit if dcmd == 'q'
+    end
+
+    def valid_cmd?(dcmd)
+      valid_inputs.include?(dcmd) || (
+        is_integer? && dcmd =~ /^\d+$/ &&
+        dcmd.to_i >= minimum_integer && (maximum_integer.nil? || dcmd.to_i <= maximum_integer)
+      )
+    end
+
+    def is_integer?
+      !minimum_integer.nil?
     end
   end
 end
